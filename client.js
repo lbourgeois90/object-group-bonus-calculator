a2a42df486ab458cd2a2906dc7705489e8646ea6
Lili Bourgeois <lbourgeois90@gmail.com>
2019-03-05 16:01:33 -0600

    bonus calculator without functioning bonusPercentage and totalBonus

--- client.js
+++ client.js
@@ -31,6 +31,52 @@ const employees = [
   }
 ];
 
+
+// * The `name` property should contain the employee's name.
+// * The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+// * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+function employeeBonusInfo( employee ) {
+
+  for ( let i = 0; i < employees.length; i++ ) {
+  
+    employees[i].totalCompensation = totalCompensation( employee );
+
+    console.log( employees[i] )
+  }
+  return employee;
+}
+
+
+
+function totalCompensation( employee ) {
+  if ( employee.reviewRating <= 2) {
+    employee.bonusPercentage = 0;
+  }//end if
+  else if (employee.reviewRating === 3) {
+    employee.bonusPercentage = .04;
+  }// end else if 3 rating
+  else if (employee.reviewRating === 4) {
+    employee.bonusPercentage = .06;
+  }// end else if 4 rating
+  else if (employee.reviewRating === 5) {
+    employee.bonusPercentage = 0.1;
+  }// end else if 5 rating
+if ((employee.employeeNumber).length === 4) {
+  employee.bonusPercentage += 0.05;
+  employee.totalBonus = (Number(employee.annualSalary) * employee.bonusPercentage );
+
+}// end if statement
+return ( (Number(employee.annualSalary) + employee.totalBonus));
+}//end percentage func
+
+
+
+console.log( employeeBonusInfo( employees[0]) );
+
+
+
+
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
@@ -40,4 +86,4 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+
